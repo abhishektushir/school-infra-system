1857ee06f666565615648b14e2fa56c3012ff3a9
abhishekt <abhishektushir799@gmail.com>
2025-08-14 20:40:12 +0530

    shadcn-ui | ui-components library setup for global access

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,32 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { BarChart3, ClipboardList } from "lucide-react";
 import React from "react";
 
 function Home() {
-  return <div></div>;
+    return <Tabs  className="w-full">
+          <TabsList className="grid w-full grid-cols-2 mb-6">
+            <TabsTrigger value="collection" className="flex items-center gap-2">
+              <ClipboardList className="h-4 w-4" />
+              Data Collection
+              <span className="text-xs">डेटा संग्रह</span>
+            </TabsTrigger>
+            <TabsTrigger value="dashboard" className="flex items-center gap-2">
+              <BarChart3 className="h-4 w-4" />
+              Dashboard
+              <span className="text-xs">डैशबोर्ड</span>
+            </TabsTrigger>
+          </TabsList>
+
+          <TabsContent value="collection">
+            {/* <DataCollection /> */}
+            abc
+          </TabsContent>
+
+          <TabsContent value="dashboard">
+            {/* <Dashboard /> */}
+            fdg
+          </TabsContent>
+        </Tabs>
 }
 
 export default Home;
