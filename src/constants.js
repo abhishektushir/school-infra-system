76799569fc8d187d8d47d32a58a380b1b87d0b17
abhishekt <abhishektushir799@gmail.com>
2025-08-16 20:01:01 +0530

    structure and logic build for dashboard

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+export const COLORS = {
+  working: "#22c55e",
+  needsRepair: "#f59e0b",
+  notAvailable: "#ef4444",
+};
+export const CONDITION = {
+  working: "Working",
+  repair: "Needs Repair",
+  availability: " Not Available",
+};
+
+export const TOASTMESSAGE = {
+  error1: "Please fill in all school details",
+  error2: "Please complete all infrastructure assessments",
+  success: "School data saved successfully!",
+};
